346dd5af17f19519a2732d45df29efe7584bad92
Jakub Valenta <jakub@datawrapper.de>
2022-08-04 22:36:45 +0200

    Add failing bad font name test

--- tests/unit/document.spec.js
+++ tests/unit/document.spec.js
@@ -1,71 +1,93 @@
 import PDFDocument from '../../lib/document';
 import { logData } from './helpers';
+import fs from 'fs';
 
 describe('PDFDocument', () => {
-  describe('font option', () => {
-    let fontSpy;
+    describe('font option', () => {
+        let fontSpy;
 
-    beforeEach(() => {
-      fontSpy = jest.spyOn(PDFDocument.prototype, 'font').mockReturnThis();
+        beforeEach(() => {
+            fontSpy = jest.spyOn(PDFDocument.prototype, 'font').mockReturnThis();
+        });
+
+        afterEach(() => {
+            fontSpy.mockRestore();
+        });
+
+        test('not defined', () => {
+            new PDFDocument();
+
+            expect(fontSpy).toBeCalledWith('Helvetica', null);
+        });
+
+        test('a string value', () => {
+            new PDFDocument({ font: 'Roboto' });
+
+            expect(fontSpy).toBeCalledWith('Roboto', null);
+        });
+
+        test('a falsy value', () => {
+            new PDFDocument({ font: null });
+            new PDFDocument({ font: false });
+            new PDFDocument({ font: '' });
+
+            expect(fontSpy).not.toBeCalled();
+        });
     });
 
-    afterEach(() => {
-      fontSpy.mockRestore();
+    describe('document info', () => {
+        test('accepts properties with value undefined', () => {
+            expect(() => new PDFDocument({ info: { Title: undefined } })).not.toThrow(
+                new TypeError("Cannot read property 'toString' of undefined"),
+            );
+        });
+
+        test('accepts properties with value null', () => {
+            expect(() => new PDFDocument({ info: { Title: null } })).not.toThrow(
+                new TypeError("Cannot read property 'toString' of null"),
+            );
+        });
     });
 
-    test('not defined', () => {
-      new PDFDocument();
+    test('metadata is present for PDF 1.4', () => {
+        let doc = new PDFDocument({ pdfVersion: '1.4' });
+        const data = logData(doc);
+        doc.end();
 
-      expect(fontSpy).toBeCalledWith('Helvetica', null);
+        let catalog = data[data.length - 28];
+
+        expect(catalog).toContain('/Metadata');
     });
 
-    test('a string value', () => {
-      new PDFDocument({ font: 'Roboto' });
+    test('metadata is NOT present for PDF 1.3', () => {
+        let doc = new PDFDocument({ pdfVersion: '1.3' });
+        const data = logData(doc);
+        doc.end();
 
-      expect(fontSpy).toBeCalledWith('Roboto', null);
+        let catalog = data[data.length - 27];
+
+        expect(catalog).not.toContain('/Metadata');
     });
+});
 
-    test('a falsy value', () => {
-      new PDFDocument({ font: null });
-      new PDFDocument({ font: false });
-      new PDFDocument({ font: '' });
+describe('FontsMixin', () => {
+    describe('font', () => {
+        test('saves a default font to _fontFamilies', () => {
+            const doc = new PDFDocument();
+            doc.font('Helvetica');
+            expect(doc._fontFamilies['Helvetica']).toBeDefined();
+        });
 
-      expect(fontSpy).not.toBeCalled();
+        test('saves a font passed as a buffer to _fontFamilies', () => {
+            const doc = new PDFDocument();
+            doc.font(fs.readFileSync('tests/fonts/Roboto-Regular.ttf'));
+            expect(doc._fontFamilies['Roboto-Regular']).toBeDefined();
+        });
+
+        test('saves a font passed as a buffer whose name is only "-" to _fontFamilies', () => {
+            const doc = new PDFDocument();
+            doc.font(fs.readFileSync('tests/fonts/Roboto-RegularBadName.ttf'));
+            expect(doc._fontFamilies['F2']).toBeDefined();
+        });
     });
-  });
-
-  describe('document info', () => {
-    test('accepts properties with value undefined', () => {
-      expect(() => new PDFDocument({ info: { Title: undefined } })).not.toThrow(
-        new TypeError("Cannot read property 'toString' of undefined")
-      );
-    });
-
-    test('accepts properties with value null', () => {
-      expect(() => new PDFDocument({ info: { Title: null } })).not.toThrow(
-        new TypeError("Cannot read property 'toString' of null")
-      );
-    });
-  });
-
-  test('metadata is present for PDF 1.4', () => {
-    let doc = new PDFDocument({pdfVersion: '1.4'});
-    const data = logData(doc);
-    doc.end();
-
-    let catalog = data[data.length-28];
-
-    expect(catalog).toContain('/Metadata');
-  });
-
-  test('metadata is NOT present for PDF 1.3', () => {
-    let doc = new PDFDocument({pdfVersion: '1.3'});
-    const data = logData(doc);
-    doc.end();
-
-    let catalog = data[data.length-27];
-
-    expect(catalog).not.toContain('/Metadata');
-  });
-
 });
